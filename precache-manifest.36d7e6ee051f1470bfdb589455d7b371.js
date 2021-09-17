4359a0eb60e055fb53c06f77483975475ebc88b1
Jungleyes1980 <gerasimosalisandratos@hotmail.com>
2021-09-17 13:05:17 +0300

    Updates

---
+++ precache-manifest.36d7e6ee051f1470bfdb589455d7b371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad3be835f4250c5232aca276f624bbe",
+    "url": "/My-Friends/index.html"
+  },
+  {
+    "revision": "4eafdd24da2b2b8487e3",
+    "url": "/My-Friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "b31d66c65b6546c6083b",
+    "url": "/My-Friends/static/css/main.d92a2a2f.chunk.css"
+  },
+  {
+    "revision": "4eafdd24da2b2b8487e3",
+    "url": "/My-Friends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Friends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31d66c65b6546c6083b",
+    "url": "/My-Friends/static/js/main.d24df562.chunk.js"
+  },
+  {
+    "revision": "dac09acbbbc15400def9",
+    "url": "/My-Friends/static/js/runtime-main.1f994e50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/My-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
